a7b42e3697309af9f1f05bee6ae8de4b08fd6e93
Junwoo Shin <shin.junwoo97@gmail.com>
2016-02-22 16:13:49 -0500

    vim | made executable and added color and submodule updating

--- vim/update.sh
+++ vim/update.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 basepath='~/.vim/bundle/'
 cd $basepath
 
@@ -7,8 +10,14 @@ plugins=($(ls))
 
 for plugin in $plugins; do
 	cd $plugin
+	echo "${GREEN} ${plugin} ${NC}"
+	echo "updating ${plugin}"
+	# updated the package itself
 	git pull origin master
+	# update submodules
+	git submodule update --remote --recursive
 	cd ..
+	echo ""
 done
 
 # TODO automatically add keying in ssh key
