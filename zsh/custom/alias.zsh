d7c1b51c24829e6937da3b3ace83eb236594cd8c
Jun Woo Shin <shin.junwoo97@gmail.com>
2016-12-14 17:21:44 -0500

    zsh | add in less aliases again

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -8,3 +8,9 @@ alias gcc="gcc-6"
 
 alias ls="ls --color=auto"
 alias fuzzy='ls -l $(find -type f | fzy)'
+
+# always show line number
+alias less='less -N'
+
+# less with syntax highlighting
+alias lesshi='LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s" less '
