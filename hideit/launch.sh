b886026c17649b71aa3b6f520bcf80aebca30c17
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-01-26 22:34:06 -0500

    hideit

---
+++ hideit/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+NAME='polybar-top_eDP1'
+
+# poll until window is available
+while true; do
+	echo 'polling'
+	xprop -name $NAME
+	if [[ $? == 0 ]]; then
+		break
+	fi
+	sleep 1
+done
+
+hideIt.sh --name $NAME --signal --direction top --peek 0 &
