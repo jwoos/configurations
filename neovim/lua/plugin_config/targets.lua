263dd89b620fc084af82868aefbdb78e8f4bf735
Jun Woo Shin <jwoos@users.noreply.github.com>
2024-01-17 18:32:34 -0500

    [neovim] fix targets config

--- neovim/lua/plugin_config/targets.lua
+++ neovim/lua/plugin_config/targets.lua
@@ -1,11 +1,4 @@
 vim.api.nvim_create_autocmd({'User'}, {
 	pattern = 'targets#mappings#user', 
-	command = [[
-call targets#mappings#extend({
-	\ 'a': {},
-	\ 'B': {},
-	\ 'b': {},
-	\ 'q': {},
-	\ })
-]]
+	command = [[call targets#mappings#extend({'a': {}, 'B': {}, 'b': {}, 'q': {}})]]
 })
