3f4f72dc9ee217863044123ac4946027edebc075
Jun Woo Shin <jwoos@users.noreply.github.com>
2024-08-07 14:27:03 -0400

    [neovim] update cmp sorting

--- neovim/lua/plugin_config/cmp.lua
+++ neovim/lua/plugin_config/cmp.lua
@@ -69,7 +69,19 @@ cmp.setup({
   }, {
     { name = 'buffer' },
     { name = 'path' },
-  })
+  }),
+  sorting = {
+		comparators = {
+			cmp.config.compare.recently_used,
+			cmp.config.compare.offset,
+			cmp.config.compare.exact,
+			cmp.config.compare.locality,
+			cmp.config.compare.kind,
+			cmp.config.compare.score,
+			cmp.config.compare.length,
+			cmp.config.compare.order,
+		},
+	}
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
