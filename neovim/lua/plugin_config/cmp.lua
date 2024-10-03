68846607bcd31dc125f8dae2cfed4d5240d84a9a
Jun Woo Shin <jwoos@users.noreply.github.com>
2024-10-03 14:34:09 -0400

    [nvim] update cmp key mapping

--- neovim/lua/plugin_config/cmp.lua
+++ neovim/lua/plugin_config/cmp.lua
@@ -33,28 +33,39 @@ cmp.setup({
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-c>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            if luasnip.expandable() then
+                luasnip.expand()
+            else
+                cmp.confirm({
+                    select = true,
+                })
+            end
+        else
+            fallback()
+        end
+    end),
+
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
+      elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
       else
         fallback()
       end
-    end, { 'i', 's' }),
+    end, { "i", "s" }),
 
     ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
+      elseif luasnip.locally_jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
       end
-    end, { 'i', 's' }),
+    end, { "i", "s" }),
   }),
   formatting = {
     fields = {"abbr", "kind", "menu"},
@@ -70,7 +81,7 @@ cmp.setup({
     { name = 'buffer' },
     { name = 'path' },
   }),
-  sorting = {
+	sorting = {
 		comparators = {
 			cmp.config.compare.recently_used,
 			cmp.config.compare.offset,
@@ -83,6 +94,7 @@ cmp.setup({
 		},
 	}
 })
+
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ '/', '?' }, {
