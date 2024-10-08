a9499691bd4845efb59fdeac329b2c67a4ff924e
Jun Woo Shin <jwoos@users.noreply.github.com>
2024-10-07 21:45:51 -0400

    [neovim] update nvim-tree auto close and add live filtering

--- neovim/lua/plugin_config/nvim_tree.lua
+++ neovim/lua/plugin_config/nvim_tree.lua
@@ -1,8 +1,38 @@
-vim.api.nvim_create_autocmd("BufEnter", {
-  nested = true,
-  callback = function()
-    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-      vim.cmd "quit"
+-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#make-q-and-bd-work-as-if-tree-was-not-visible
+-- Make :bd and :q behave as usual when tree is visible
+vim.api.nvim_create_autocmd({'BufEnter', 'QuitPre'}, {
+  nested = false,
+  callback = function(e)
+    local tree = require('nvim-tree.api').tree
+
+    -- Nothing to do if tree is not opened
+    if not tree.is_visible() then
+      return
+    end
+
+    -- How many focusable windows do we have? (excluding e.g. incline status window)
+    local winCount = 0
+    for _,winId in ipairs(vim.api.nvim_list_wins()) do
+      if vim.api.nvim_win_get_config(winId).focusable then
+        winCount = winCount + 1
+      end
+    end
+
+    -- We want to quit and only one window besides tree is left
+    if e.event == 'QuitPre' and winCount == 2 then
+      vim.api.nvim_cmd({cmd = 'qall'}, {})
+    end
+
+    -- :bd was probably issued an only tree window is left
+    -- Behave as if tree was closed (see `:h :bd`)
+    if e.event == 'BufEnter' and winCount == 1 then
+      -- Required to avoid "Vim:E444: Cannot close last window"
+      vim.defer_fn(function()
+        -- close nvim-tree: will go to the last buffer used before closing
+        tree.toggle({find_file = true, focus = true})
+        -- re-open nivm-tree
+        tree.toggle({find_file = true, focus = false})
+      end, 10)
     end
   end
 })
@@ -43,6 +73,8 @@ local function on_attach(bufnr)
   vim.keymap.set('n', '<', api.tree.change_root_to_parent, opts('Up'))
   vim.keymap.set('n', '>', api.tree.change_root_to_node, opts('CD'))
   vim.keymap.set('n', 'q', api.tree.close, opts('Close'))
+  vim.keymap.set("n", 'f', api.live_filter.start, opts('Live Filter: Start'))
+	vim.keymap.set("n", 'F', api.live_filter.clear, opts('Live Filter: Clear'))
 end
 
 require'nvim-tree'.setup {
