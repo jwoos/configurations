5e2c0dd7adcd0d4a8cd0c1afaaa256f93ba4fa0c
Jun Woo Shin <shin.junwoo97@gmail.com>
2024-12-18 16:47:37 -0500

    [neovim] add linewise substitute

--- neovim/lua/plugin_config/substitute.lua
+++ neovim/lua/plugin_config/substitute.lua
@@ -3,6 +3,7 @@ local sub = require('substitute')
 sub.setup({})
 
 vim.keymap.set("n", "s", sub.operator, { noremap = true })
+vim.keymap.set("n", "ss", sub.line, { noremap = true })
 vim.keymap.set("n", "S", sub.eol, { noremap = true })
 vim.keymap.set("x", "s", sub.visual, { noremap = true })
 
