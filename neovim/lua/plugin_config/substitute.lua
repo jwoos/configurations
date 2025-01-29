4c497996f323f7643e1d1a0b48b389133c25dbb2
Jun Woo Shin <jwoos@users.noreply.github.com>
2025-01-29 16:30:14 -0500

    [neovim] update substitute exchange mapping

--- neovim/lua/plugin_config/substitute.lua
+++ neovim/lua/plugin_config/substitute.lua
@@ -7,5 +7,5 @@ vim.keymap.set("n", "ss", sub.line, { noremap = true })
 vim.keymap.set("n", "S", sub.eol, { noremap = true })
 vim.keymap.set("x", "s", sub.visual, { noremap = true })
 
-vim.keymap.set("n", "<leader>x", require('substitute.exchange').operator, { noremap = true })
-vim.keymap.set("x", "<leader>x", require('substitute.exchange').visual, { noremap = true })
+vim.keymap.set("n", "sx", require('substitute.exchange').operator, { noremap = true })
+vim.keymap.set("x", "sx", require('substitute.exchange').visual, { noremap = true })
