5343b9d419e853b95f5f894dfbfbe7ff35073b71
Jun Woo Shin <jwoos@users.noreply.github.com>
2025-08-20 09:52:20 -0400

    [neovim] update color winsep config

--- neovim/lua/plugin_config/colorful_winsep.lua
+++ neovim/lua/plugin_config/colorful_winsep.lua
@@ -1,10 +1,7 @@
 require('colorful-winsep').setup({
-        no_exec_files = {'NvimTree'},
-        highlight = {
-                fg = require('kanagawa.colors').setup({ theme = 'dragon' }).palette.lightBlue,
-                bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg,
-        },
-        animate = {
-                enabled = false
-        }
+	no_exec_files = {'NvimTree'},
+	highlight = require('kanagawa.colors').setup({ theme = 'dragon' }).palette.lightBlue,
+	animate = {
+		enabled = false
+	}
 })
