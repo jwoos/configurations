e8d01203d9d40f9829a0b427e10bbe4d7651797b
Jun Woo Shin <jwoos@users.noreply.github.com>
2026-01-02 13:03:58 -0500

    [neovim] remove spread config

--- neovim/lua/plugin_config/spread.lua
+++
@@ -1,9 +0,0 @@
-local spread = require('spread')
-
-local opts = {
-  silent = true,
-  noremap = true
-}
-
-vim.keymap.set('n', 'bM', spread.out, opts)
-vim.keymap.set('n', 'bm', spread.combine, opts)
