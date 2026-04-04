15e9a2ba79c50d118b713f2f76311bb98e14b9ec
Jun Woo Shin <shin.junwoo97@gmail.com>
2026-04-04 18:49:08 -0400

    [neovim] adjust neoclip mapping

--- neovim/lua/plugin_config/neoclip.lua
+++ neovim/lua/plugin_config/neoclip.lua
@@ -4,10 +4,21 @@ require('neoclip').setup({
 	enable_persistent_history = true,
 	db_path = vim.fn.stdpath("data") .. "/databases/neoclip.sqlite3",
 	preview = true,
+	on_select = {
+		move_to_front = true,
+		close_telescope = true,
+	},
+	on_paste = {
+		set_reg = false,
+		move_to_front = true,
+		close_telescope = true,
+	},
 })
 
 local fzf_handler = require('neoclip.fzf')
-local register_specific_handler = function()
+
+vim.keymap.set('n', '<c-y>', fzf_handler, {noremap = true})
+vim.keymap.set('n', '<a-y>', function()
   vim.ui.input({prompt = "Register to yank into: "}, function(input)
 		if input == nil then
 			fzf_handler()
@@ -16,7 +27,24 @@ local register_specific_handler = function()
 
     fzf_handler({input})
   end)
+end, {noremap = true})
+
+local put_from_reg = function(reg)
+	local str = vim.fn.getreg(reg)
+	vim.api.nvim_paste(str, false, -1)
 end
 
-vim.keymap.set('n', '<c-p>', fzf_handler, {noremap = true})
-vim.keymap.set('n', '<a-p>', register_specific_handler, {noremap = true})
+-- vim.keymap.set('n', '<c-p>', function()
+-- 	fzf_handler()
+-- 	-- vim.cmd('normal! p')
+-- end, {noremap = true})
+-- vim.keymap.set('n', '<a-p>', function()
+--   vim.ui.input({prompt = "Register to yank into: "}, function(input)
+-- 		if input == nil then
+-- 			fzf_handler()
+-- 		else
+-- 			fzf_handler({input})
+-- 		end
+-- 		vim.cmd('normal! "'..input..'p')
+--   end)
+-- end, {noremap = true})
